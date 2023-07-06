34f6794bdb63fb45abace82a522fc8ad5ebd1886
jvftw <jvmbworks@gmail.com>
2023-07-06 00:27:21 -0400

    queries 6,7,8

---
+++ query6.sql
@@ -0,0 +1,10 @@
+select 
+	case
+		when ibge.gender = 'male' then 'homens'
+		when ibge.gender = 'female' then 'mulheres'
+		end as "gênero",
+	count(*) as "leads (#)"
+from sales.customers as cus
+left join temp_tables.ibge_genders as ibge
+	on lower(cus.first_name) = lower(ibge.first_name)
+group by ibge.gender
---
+++ query8.sql
@@ -0,0 +1,12 @@
+select
+	case
+		when datediff('years', birth_date, current_date) < 20 then '0-20'
+		when datediff('years', birth_date, current_date) < 40 then '20-40'
+		when datediff('years', birth_date, current_date) < 60 then '40-60'
+		when datediff('years', birth_date, current_date) < 80 then '60-80'
+		else '80+' end "faixa etária",
+		count(*)::float/(select count(*) from sales.customers) as "leads (%)"
+		
+from sales.customers
+group by "faixa etária"
+order by "faixa etária" desc
